71667c8af31975b909a8578f5c3a67042a594824
Eugene Leonovich <gen.work@gmail.com>
2012-06-04 00:02:08 +0200

    Add check for loaded extension to IgbinarySerializerTest

--- tests/Phive/Tests/Serializer/IgbinarySerializerTest.php
+++ tests/Phive/Tests/Serializer/IgbinarySerializerTest.php
@@ -6,6 +6,15 @@ use Phive\Serializer\IgbinarySerializer;
 
 class IgbinarySerializerTest extends AbstractSerializerTest
 {
+    public function setUp()
+    {
+        if (!extension_loaded('igbinary')) {
+            $this->markTestSkipped('IgbinarySerializer requires the php "igbinary" extension.');
+        }
+
+        parent::setUp();
+    }
+
     protected function createSerializer()
     {
         return new IgbinarySerializer();
