8c0f3a3494d02086cdadce357ce35a845b08e98a
Eugene Leonovich <gen.work@gmail.com>
2012-07-15 00:10:20 +0200

    Update install_deps.sh

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+PHP_INI_FILE=$(php -r "echo php_ini_loaded_file();")
+
+# mongo
 wget http://pecl.php.net/get/mongo-1.2.7.tgz > /dev/null 2>&1
 tar -xzf mongo-1.2.7.tgz > /dev/null 2>&1
 sh -c "cd mongo-1.2.7 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
-echo "extension=mongo.so" >> $(php -r "echo php_ini_loaded_file();")
+echo "extension=mongo.so" >> $PHP_INI_FILE
+
+# redis
+wget https://github.com/nicolasff/phpredis/tarball/2.2.1 > /dev/null 2>&1
+tar -xzf 2.2.1 > /dev/null 2>&1
+sh -c "cd nicolasff-phpredis-250e81b && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
+echo "extension=redis.so" >> $PHP_INI_FILE
