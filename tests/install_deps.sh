780bd22e7c07d78350df71a68b697d49dc0e9670
Eugene Leonovich <gen.work@gmail.com>
2012-07-15 00:29:07 +0200

    Update install_deps.sh

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -8,7 +8,12 @@ tar -xzf mongo-1.2.7.tgz > /dev/null 2>&1
 sh -c "cd mongo-1.2.7 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
 echo "extension=mongo.so" >> $PHP_INI_FILE
 
-# redis
+# redis-server
+wget http://redis.googlecode.com/files/redis-2.6.0-rc5.tar.gz
+tar -xzf redis-2.6.0-rc5.tar.gz > /dev/null 2>&1
+sh -c "cd redis-2.6.0-rc5 && make && ./src/redis-server" > /dev/null 2>&1
+
+# phpredis
 wget https://github.com/nicolasff/phpredis/tarball/2.2.1 > /dev/null 2>&1
 tar -xzf 2.2.1 > /dev/null 2>&1
 sh -c "cd nicolasff-phpredis-250e81b && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
