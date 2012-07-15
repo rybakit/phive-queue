5715ce2e07f042661accf1a0d8e07a3a5f9e912c
Eugene Leonovich <gen.work@gmail.com>
2012-07-15 13:23:36 +0200

    Update .travis.yml

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -2,7 +2,7 @@
 
 PHP_INI_FILE=$(php -r "echo php_ini_loaded_file();")
 
-# redis-server
+# redis-server v2.6
 sudo add-apt-repository -y ppa:rwky/redis-unstable > /dev/null 2>&1
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install redis-server > /dev/null 2>&1
