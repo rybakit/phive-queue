8975a501e84a585aaee5cf4ef17292f155a5f27b
Eugene Leonovich <gen.work@gmail.com>
2012-07-15 11:55:19 +0200

    Test travic-ci

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -2,11 +2,11 @@
 
 PHP_INI_FILE=$(php -r "echo php_ini_loaded_file();")
 
-# mongo
-wget http://pecl.php.net/get/mongo-1.2.7.tgz > /dev/null 2>&1
-tar -xzf mongo-1.2.7.tgz > /dev/null 2>&1
-sh -c "cd mongo-1.2.7 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
-echo "extension=mongo.so" >> $PHP_INI_FILE
+## mongo
+#wget http://pecl.php.net/get/mongo-1.2.7.tgz > /dev/null 2>&1
+#tar -xzf mongo-1.2.7.tgz > /dev/null 2>&1
+#sh -c "cd mongo-1.2.7 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
+#echo "extension=mongo.so" >> $PHP_INI_FILE
 
 
 # redis-server
@@ -21,5 +21,6 @@ cd redis-2.6.0-rc5 && make && ./src/redis-server
 # phpredis
 wget https://github.com/nicolasff/phpredis/tarball/2.2.1 > /dev/null 2>&1
 tar -xzf 2.2.1 > /dev/null 2>&1
-sh -c "cd nicolasff-phpredis-250e81b && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
+#sh -c "cd nicolasff-phpredis-250e81b && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
+cd nicolasff-phpredis-250e81b && phpize && ./configure && make && sudo make install
 echo "extension=redis.so" >> $PHP_INI_FILE
