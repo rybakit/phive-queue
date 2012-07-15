3e31a9b4708b66c82c3bc2a1617e47b30b45cdbe
Eugene Leonovich <gen.work@gmail.com>
2012-07-15 12:24:05 +0200

    Test travic-ci

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -10,17 +10,17 @@ PHP_INI_FILE=$(php -r "echo php_ini_loaded_file();")
 
 
 # redis-server
-redis-server -v
+#redis-server -v
 sudo service redis-server stop
 
 wget http://redis.googlecode.com/files/redis-2.6.0-rc5.tar.gz > /dev/null 2>&1
 tar -xzf redis-2.6.0-rc5.tar.gz > /dev/null 2>&1
-#sh -c "cd redis-2.6.0-rc5 && make && ./src/redis-server &" > /dev/null 2>&1
-cd redis-2.6.0-rc5 && make && ./src/redis-server &
+sh -c "cd redis-2.6.0-rc5 && make && ./src/redis-server &" > /dev/null 2>&1
+#cd redis-2.6.0-rc5 && make && ./src/redis-server &
 
 # phpredis
 wget https://github.com/nicolasff/phpredis/tarball/2.2.1 > /dev/null 2>&1
 tar -xzf 2.2.1 > /dev/null 2>&1
-#sh -c "cd nicolasff-phpredis-250e81b && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
-cd nicolasff-phpredis-250e81b && phpize && ./configure && make && sudo make install
+sh -c "cd nicolasff-phpredis-250e81b && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
+#cd nicolasff-phpredis-250e81b && phpize && ./configure && make && sudo make install
 echo "extension=redis.so" >> $PHP_INI_FILE
