f154af81b80a5ed771bea973d435119ea98cc3ed
Eugene Leonovich <gen.work@gmail.com>
2012-07-15 13:18:49 +0200

    Update install_deps.sh

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -2,20 +2,19 @@
 
 PHP_INI_FILE=$(php -r "echo php_ini_loaded_file();")
 
-## mongo
-#wget http://pecl.php.net/get/mongo-1.2.10.tgz > /dev/null 2>&1
-#tar -xzf mongo-1.2.10.tgz > /dev/null 2>&1
-#sh -c "cd mongo-1.2.10 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
-#echo "extension=mongo.so" >> $PHP_INI_FILE
-
-
 # redis-server
-sudo add-apt-repository -y ppa:rwky/redis-unstable
-sudo apt-get update
-sudo apt-get install redis-server
+sudo add-apt-repository -y ppa:rwky/redis-unstable > /dev/null 2>&1
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install redis-server > /dev/null 2>&1
 
 # phpredis
 wget https://github.com/nicolasff/phpredis/tarball/f3dff08cfaf5d6a7a78bd87e70ee19c92f0ad27d > /dev/null 2>&1
 tar -xzf f3dff08cfaf5d6a7a78bd87e70ee19c92f0ad27d > /dev/null 2>&1
 sh -c "cd nicolasff-phpredis-f3dff08 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
 echo "extension=redis.so" >> $PHP_INI_FILE
+
+## mongo
+wget http://pecl.php.net/get/mongo-1.2.10.tgz > /dev/null 2>&1
+tar -xzf mongo-1.2.10.tgz > /dev/null 2>&1
+sh -c "cd mongo-1.2.10 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
+echo "extension=mongo.so" >> $PHP_INI_FILE
