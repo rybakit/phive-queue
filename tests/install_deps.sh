55fad4240211d786c5ae706c4370c93f5cd2db41
Eugene Leonovich <gen.work@gmail.com>
2012-07-18 10:52:44 +0300

    Tweak install_deps.sh

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 PHP_INI_FILE=$(php -r "echo php_ini_loaded_file();")
 
@@ -13,7 +13,7 @@ tar -xzf f3dff08cfaf5d6a7a78bd87e70ee19c92f0ad27d > /dev/null 2>&1
 sh -c "cd nicolasff-phpredis-f3dff08 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
 echo "extension=redis.so" >> $PHP_INI_FILE
 
-## mongo
+# mongo
 wget http://pecl.php.net/get/mongo-1.2.10.tgz > /dev/null 2>&1
 tar -xzf mongo-1.2.10.tgz > /dev/null 2>&1
 sh -c "cd mongo-1.2.10 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
