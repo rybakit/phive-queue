71677852fbc7a4b63a27501cee34a5c59260aedd
Eugene Leonovich <gen.work@gmail.com>
2012-07-19 11:56:13 +0200

    Fix CS

--- src/Phive/Queue/QueueInterface.php
+++ src/Phive/Queue/QueueInterface.php
@@ -8,25 +8,25 @@ interface QueueInterface extends \Countable
      * @param mixed                     $item
      * @param \DateTime|string|int|null $eta
      */
-    function push($item, $eta = null);
+    public function push($item, $eta = null);
 
     /**
      * @return mixed|bool false if queue is empty, an item otherwise
      */
-    function pop();
+    public function pop();
 
     /**
      * @param int $limit
      * @param int $skip
      *
-     * @throws \OutOfRangeException
-     *
      * @return \Iterator
+     *
+     * @throws \OutOfRangeException
      */
-    function peek($limit = 1, $skip = 0);
+    public function peek($limit = 1, $skip = 0);
 
     /**
      * Removes all items from the queue.
      */
-    function clear();
+    public function clear();
 }
