f92cd1a102fd2ef32cd6d5c54e6dbad85f4404b9
Eugene Leonovich <gen.work@gmail.com>
2012-08-18 22:26:21 +0200

    Debug install_deps.sh script

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -3,14 +3,14 @@
 PHP_INI_FILE=$(php -r "echo php_ini_loaded_file();")
 
 # redis-server v2.6
-sudo add-apt-repository -y ppa:rwky/redis-unstable #> /dev/null 2>&1
-sudo apt-get update #> /dev/null 2>&1
-sudo apt-get install redis-server #> /dev/null 2>&1
+sudo add-apt-repository -y ppa:rwky/redis-unstable > /dev/null 2>&1
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install redis-server > /dev/null 2>&1
 
 # phpredis
 wget https://github.com/nicolasff/phpredis/tarball/22f455593a9027cf935d37d70e198de225283d2e > /dev/null 2>&1
-tar -xzf 22f455593a9027cf935d37d70e198de225283d2e > /dev/null 2>&1
-sh -c "cd nicolasff-phpredis-f3dff08 && phpize && ./configure && make && sudo make install" > /dev/null 2>&1
+tar -xzf 22f455593a9027cf935d37d70e198de225283d2e #> /dev/null 2>&1
+sh -c "cd nicolasff-phpredis-f3dff08 && phpize && ./configure && make && sudo make install" #> /dev/null 2>&1
 echo "extension=redis.so" >> $PHP_INI_FILE
 
 # mongo
