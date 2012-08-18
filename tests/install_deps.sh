31a0e74096b1c4add69940a9ac979bf5b301b497
Eugene Leonovich <gen.work@gmail.com>
2012-08-18 22:16:14 +0200

    Debug install_deps.sh script

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -3,9 +3,9 @@
 PHP_INI_FILE=$(php -r "echo php_ini_loaded_file();")
 
 # redis-server v2.6
-sudo add-apt-repository -y ppa:rwky/redis-unstable > /dev/null 2>&1
-sudo apt-get update > /dev/null 2>&1
-sudo apt-get install redis-server > /dev/null 2>&1
+sudo add-apt-repository -y ppa:rwky/redis-unstable #> /dev/null 2>&1
+sudo apt-get update #> /dev/null 2>&1
+sudo apt-get install redis-server #> /dev/null 2>&1
 
 # phpredis
 wget https://github.com/nicolasff/phpredis/tarball/22f455593a9027cf935d37d70e198de225283d2e > /dev/null 2>&1
