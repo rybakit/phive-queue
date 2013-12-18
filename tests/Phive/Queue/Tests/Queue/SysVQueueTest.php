be00e00adfbd8f7a6b681bf0d5cbbb35acb084db
Eugene Leonovich <gen.work@gmail.com>
2013-12-18 13:35:30 +0100

    Fix SysVQueueTest

--- tests/Phive/Queue/Tests/Queue/SysVQueueTest.php
+++ tests/Phive/Queue/Tests/Queue/SysVQueueTest.php
@@ -9,7 +9,7 @@ class SysVQueueTest extends AbstractQueueTest
     public function createQueue()
     {
         if (!extension_loaded('sysvmsg')) {
-            throw new \RuntimeException('The "sysvmsg" extension is not loaded.');
+            $this->markTestSkipped('The "sysvmsg" extension is not loaded.');
         }
 
         return new SysVQueue(0xDEADBEEF);
