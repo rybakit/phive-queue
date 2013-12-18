53349969b6b1e24359db14c933142aa1872fa03b
Eugene Leonovich <gen.work@gmail.com>
2013-12-18 12:24:15 +0100

    Check if the "sysvmsg" extension was loaded before running SysVQueueTest

--- tests/Phive/Queue/Tests/Queue/SysVQueueTest.php
+++ tests/Phive/Queue/Tests/Queue/SysVQueueTest.php
@@ -8,6 +8,10 @@ class SysVQueueTest extends AbstractQueueTest
 {
     public function createQueue()
     {
+        if (!extension_loaded('sysvmsg')) {
+            throw new \RuntimeException('The "sysvmsg" extension is not loaded.');
+        }
+
         return new SysVQueue(0xDEADBEEF);
     }
 }
