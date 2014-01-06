e988612319d30171ec59ec137d84b6cfeac63b69
Eugene Leonovich <gen.work@gmail.com>
2014-01-07 00:39:48 +0100

    Switch to PSR-4 autoloading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 
 spl_autoload_register(function($class) {
+    if ('Phive\\Queue\\' === substr($class, 0, 12)) {
+        $class = substr($class, 12);
+    }
+    if ('Tests\\' === substr($class, 0, 6)) {
+        $class = substr($class, 6);
+    }
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     foreach (array('src', 'tests') as $dirPrefix) {
