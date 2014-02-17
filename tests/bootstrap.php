02fd2f90b3a6a115f730de391722e583b965a9f4
Eugene Leonovich <gen.work@gmail.com>
2014-02-17 22:26:31 +0100

    Simplify tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,22 +1,4 @@
 <?php
 
-spl_autoload_register(function($class) {
-    if ('Phive\\Queue\\' === substr($class, 0, 12)) {
-        $class = substr($class, 12);
-    }
-    if ('Tests\\' === substr($class, 0, 6)) {
-        $class = substr($class, 6);
-    }
-    $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    foreach (['src', 'tests'] as $dirPrefix) {
-        $file = __DIR__.'/../'.$dirPrefix.'/'.$path.'.php';
-        if (file_exists($file)) {
-            require_once $file;
-
-            return true;
-        }
-    }
-
-    return false;
-});
+$loader = require __DIR__.'/../vendor/autoload.php';
+$loader->addPsr4('Phive\\Queue\\Tests\\', __DIR__);
