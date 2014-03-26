4389555dbd29fbfa606ff2748c572feff35d0829
Eugene Leonovich <gen.work@gmail.com>
2014-03-26 09:53:08 +0100

    Add missing PHPDoc to the Queue interface

--- src/Queue/Queue.php
+++ src/Queue/Queue.php
@@ -5,12 +5,16 @@ namespace Phive\Queue\Queue;
 interface Queue extends \Countable
 {
     /**
-     * @param mixed $item
-     * @param mixed $eta
+     * Adds an item to the queue.
+     *
+     * @param mixed $item   An item to be added.
+     * @param mixed $eta    The earliest time that an item can be popped.
      */
     public function push($item, $eta = null);
 
     /**
+     * Removes an item from the queue and returns it.
+     *
      * @return mixed
      *
      * @throws \Phive\Queue\Exception\NoItemAvailableException
