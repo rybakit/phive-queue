4b68370f2aa14d08a576de4785652e7799c4970d
Eugene Leonovich <gen.work@gmail.com>
2014-09-08 14:52:39 +0200

    Simplify MockPdo class

--- tests/Queue/Pdo/MockPdo.php
+++ tests/Queue/Pdo/MockPdo.php
@@ -4,7 +4,6 @@ namespace Phive\Queue\Tests\Queue\Pdo;
 
 class MockPdo extends \PDO
 {
-    public $errorMode = self::ERRMODE_EXCEPTION;
     public $driverName;
 
     public function __construct()
@@ -13,15 +12,10 @@ class MockPdo extends \PDO
 
     public function getAttribute($attribute)
     {
-        switch ($attribute) {
-            case self::ATTR_ERRMODE:
-                return $this->errorMode;
-
-            case self::ATTR_DRIVER_NAME:
-                return $this->driverName;
-
-            default:
-                return parent::getAttribute($attribute);
+        if (self::ATTR_DRIVER_NAME === $attribute) {
+            return $this->driverName;
         }
+
+        return parent::getAttribute($attribute);
     }
 }
